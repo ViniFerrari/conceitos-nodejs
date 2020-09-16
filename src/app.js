3a0854eb7339c57a73f3153a3d5c57875a430449
Vinicius Ferrari Oliveira <viniferrari1999@gmail.com>
2020-09-15 21:30:11 -0300

    Project CRUD, Like

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,71 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs  } = request.body
+
+  const repositoryId = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryId === -1) {
+    return response.status(400).json({ errror: 'Esse repositório não existe.' });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryId].likes,
+  }
+
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryId = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryId >= 0) {
+    repositories.splice(repositoryId, 1);
+  } else {
+    return response.status(400).json({ error: 'REpositório não existe.' });
+  }
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if (!repository) {
+    return response.status(400).send();
+  }
+
+  repository.likes++;
+  return response.json(repository);
 });
 
 module.exports = app;
